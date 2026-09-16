6b469d7ddc00abcfef62260ca787849abf9e84cd
Boris Araujo <48392819+borisaraujo@users.noreply.github.com>
2026-09-16 15:54:57 -0300

    Homol (#29)

--- src/components/logos/index.jsx
+++ src/components/logos/index.jsx
@@ -6,17 +6,17 @@ function Logos() {
   return (
 <>
       <div className="logos">
-          <div>
-          <img className="max-w-[100px] md:max-w-[150px]"  src={logo}/>
+          <div className="logo-box">
+          <img className="max-w-[50px] md:max-w-[70px]"  src={logo}/>
           </div>
             <div className="info hidden sm:block">
-                <h2>Painel de Últimas Posições dos Veículos - SPPO e BRT</h2>
+                <h2>Painel de Últimas Posições dos Veículos - SPPO, RIO e BRT</h2>
                 Dados atualizados a cada 1 minuto. Última atualização: {formattedHora}
             </div>
-        
+
       </div>
       <div className="absolute info  block z-[10000] sm:hidden w-full bottom-0">
-        <h2>Painel de Últimas Posições dos Veículos - SPPO e BRT</h2>
+        <h2>Painel de Últimas Posições dos Veículos - SPPO, RIO e BRT</h2>
         Dados atualizados a cada 1 minuto. Última atualização: {formattedHora}
       </div>
       
